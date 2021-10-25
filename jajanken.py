d9e58b63fe3ab5c3f4f4ffd8de0c349be7956f7c
Temituoyo Ugborogho <troytuoyo@gmail.com>
2021-10-24 21:59:26 -0400

    Game is now playable need to check for bugs and add comments

--- jajanken.py
+++ jajanken.py
@@ -4,21 +4,23 @@ def game_mode(rounds):
     try:
         ## 3 rounds
         if (rounds == 3):
-            print("Best out of 3!")
+            print("Best out of 3!\n")
             ## 2 wins required to win
             win_in = 2
         ## 5 rounds
         elif (rounds == 5):
-            print("Best out of 5!")
+            print("Best out of 5!\n")
             ## 3 wins required to win
             win_in = 3
+        else:
+            print("INVALID! Either 3 or 5 rounds")
     except:
-        print("Invalid Input!\nEnter 3 or 5 to select number of rounds")
+        print("Invalid Input!\nEnter 3 or 5 to select number of rounds\n")
 
-    return win_in
+    return win_in    
 
 #CPU turn
-def CPU_play(select):
+def CPU_play():
     x = random.randint(1, 3)
     if (x == 1):
         CPU = "ROCK"
@@ -33,35 +35,90 @@ def CPU_play(select):
 def play(choice, opponent, user_score, CPU_score):
     if (choice == "ROCK"):
         if (opponent == "ROCK"):
-            print("Draw")
+            print("Draw\n")
         elif (opponent == "PAPER"):
+            print("CPU wins round!\n")
             CPU_score += 1
         elif (opponent == "SCISSORS"):
+            print("You win this round\n")
             user_score += 1
     elif (choice == "PAPER"):
         if (opponent == "ROCK"):
+            print("You win this round\n")
             user_score += 1
         elif (opponent == "PAPER"):
-            print("Draw")
+            print("Draw\n")
         elif (opponent == "SCISSORS"):
+            print("CPU wins round!\n")
             CPU_score += 1
     elif (choice == "SCISSORS"):
         if (opponent == "ROCK"):
+            print("CPU wins round!\n")
             CPU_score += 1
         elif (opponent == "PAPER"):
+            print("You win this round\n")
             user_score += 1
         elif (opponent == "SCISSORS"):
-            print("Draw")
+            print("Draw\n")
 
     return user_score, CPU_score
 
 def winner(user_score, CPU_score, win_in):
     if (user_score == win_in):
-        print("You win")
+        print("CONGRATULATIONS! You win")
+        return True
     elif (CPU_score == win_in):
-        print("Better luck next time. Try again?")
+        print("GAME OVER\nBetter luck next time. Try again?\n")
+        return True
+    else:
+        return False
 
 def main():
-    return
+    print("WELCOME TO ROCK PAPER SCISSORS.\n\nEnter Q at anytime to quit and R to restart\n")
+    while True:
+        try:
+            round_count = int(input("How many rounds do you wish to play.\nEnter 3 or 5 for No.of.rounds: "))
+            to_win = game_mode(round_count)
+        except:
+            print("Invalid input\n")
+        else:
+            break
+    user_score = 0
+    CPU_score = 0
+    while True:
+        response = input("Rock, Paper, Scissors, shoot!: ")
+        response = response.upper()
+        if (response == "Q"):
+            print("Bye!\n")
+            break
+        elif (response == "R"):
+            print("NEW GAME\n")
+            user_score = 0
+            CPU_score = 0
+        else:
+            user_score, CPU_score = play(response, CPU_play(), user_score, CPU_score)
+            check_winner = winner(user_score, CPU_score, to_win)
+            if (check_winner):
+                while True:
+                    try:
+                        new = input("Enter Q for Quit or R to Restart: ")
+                        new = new.upper()
+                        if (new == "Q" or new == "R"):
+                            response = new
+                        else:
+                            raise
+                    except:
+                        print("Invalid!\n")
+                    else:
+                        break
+                if (response == "R"):
+                    print("NEW GAME\n")
+                    user_score = 0
+                    CPU_score = 0
+                elif (response == "Q"):
+                    print("Bye!\n")
+                    break
+        
 
-##Incomplete
+main()
+##Gotta add comments to code and review code
