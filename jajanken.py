5d961f08bf3920ceed09aaba2c0a3a2724e9f954
Temituoyo Ugborogho <troytuoyo@gmail.com>
2021-10-24 20:43:19 -0400

    Created game functionality. Game incomplete

--- jajanken.py
+++ jajanken.py
@@ -2,16 +2,20 @@ import random
 ## How many rounds does user want to play
 def game_mode(rounds):
     try:
+        ## 3 rounds
         if (rounds == 3):
             print("Best out of 3!")
-            y = 3
+            ## 2 wins required to win
+            win_in = 2
+        ## 5 rounds
         elif (rounds == 5):
             print("Best out of 5!")
-            y = 5
+            ## 3 wins required to win
+            win_in = 3
     except:
         print("Invalid Input!\nEnter 3 or 5 to select number of rounds")
 
-    return y
+    return win_in
 
 #CPU turn
 def CPU_play(select):
@@ -26,9 +30,38 @@ def CPU_play(select):
     return CPU
 
 
-def score_count():
-    
-    return
+def play(choice, opponent, user_score, CPU_score):
+    if (choice == "ROCK"):
+        if (opponent == "ROCK"):
+            print("Draw")
+        elif (opponent == "PAPER"):
+            CPU_score += 1
+        elif (opponent == "SCISSORS"):
+            user_score += 1
+    elif (choice == "PAPER"):
+        if (opponent == "ROCK"):
+            user_score += 1
+        elif (opponent == "PAPER"):
+            print("Draw")
+        elif (opponent == "SCISSORS"):
+            CPU_score += 1
+    elif (choice == "SCISSORS"):
+        if (opponent == "ROCK"):
+            CPU_score += 1
+        elif (opponent == "PAPER"):
+            user_score += 1
+        elif (opponent == "SCISSORS"):
+            print("Draw")
 
+    return user_score, CPU_score
+
+def winner(user_score, CPU_score, win_in):
+    if (user_score == win_in):
+        print("You win")
+    elif (CPU_score == win_in):
+        print("Better luck next time. Try again?")
+
+def main():
+    return
 
 ##Incomplete
